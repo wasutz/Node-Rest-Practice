dd86a703afd268a2881370ec90818b71c91e6c5b
Wasut <wasut_pao@hotmail.com>
2016-12-13 20:24:04 +0700

    add required routes.js

--- app.js
+++ app.js
@@ -5,20 +5,10 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/bookstore');
 
-Book = require('./models/book.js');
+require('./routes')(app);
 
 app.get('/', function(request, response){
 	response.send('Please use api in path /api/v1/books');
-});
-
-app.get('/api/v1/books', function(request, response){
-	Book.getBooks(function(err, books){
-		if(err){
-			throw err;
-		}
-
-		response.json(books);
-	});
 });
 
 app.listen(3000, function(){
