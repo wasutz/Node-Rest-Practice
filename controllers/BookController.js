ce9482a304410cbc6a68c8f90ab8a969fdc03e96
Wasut <wasut_pao@hotmail.com>
2016-12-13 22:37:35 +0700

    add controller of api post put delete book

--- controllers/BookController.js
+++ controllers/BookController.js
@@ -1,4 +1,4 @@
-Book = require('../models/book.js');
+Book = require('../models/Book.js');
 
 exports.findAll = function(request, response) {
 	Book.findAll(function(err, books){
@@ -7,5 +7,52 @@ exports.findAll = function(request, response) {
 		}
 
 		response.json(books);
+	});
+};
+
+exports.findById = function(request, response) {
+	Book.findById(request.params.id, function(err, book){
+		if(err){
+			throw err;
+		}
+
+		response.json(book);
+	});
+};
+
+exports.createBook = function(request, response) {
+	var book = request.body;
+
+	Book.createBook(book, function(err, book){
+		if(err){
+			throw err;
+		}
+
+		response.json(book);
+	});
+};
+
+exports.updateBook = function(request, response) {
+	var id = request.params.id;
+	var book = request.body;
+
+	Book.updateBook(id, book, function(err, book){
+		if(err){
+			response.status(404).send('Not found');
+		}
+
+		response.json(book);
+	});
+};
+
+exports.deleteBook = function(request, response) {
+	var id = request.params.id;
+
+	Book.deleteBook(id, function(err, book){
+		if(err){
+			throw err;
+		}
+
+		response.status(204).send('No Content');
 	});
 };
