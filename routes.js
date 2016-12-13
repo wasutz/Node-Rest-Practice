2fb7c11c82aedb2db48895865b7e8abd6e752032
Wasut <wasut_pao@hotmail.com>
2016-12-13 22:36:30 +0700

    add route get one, post, put, delete

--- routes.js
+++ routes.js
@@ -3,4 +3,8 @@ module.exports = function(app){
     var bookController = require('./controllers/BookController');
 
     app.get('/books', bookController.findAll);
+    app.get('/books/:id', bookController.findById);
+    app.post('/books', bookController.createBook);
+    app.put('/books/:id', bookController.updateBook);
+    app.delete('/books/:id', bookController.deleteBook);
 }
