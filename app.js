9569bf9adf64b3485f760bb02884b8f871b15733
Wasut <wasut_pao@hotmail.com>
2016-12-13 19:28:33 +0700

    add require model book, add route api /api/v1/books

--- app.js
+++ app.js
@@ -3,8 +3,22 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/bookstore');
+
+Book = require('./models/book.js');
+
 app.get('/', function(request, response){
 	response.send('Please use api in path /api/v1/books');
+});
+
+app.get('/api/v1/books', function(request, response){
+	Book.getBooks(function(err, books){
+		if(err){
+			throw err;
+		}
+
+		response.json(books);
+	});
 });
 
 app.listen(3000, function(){
