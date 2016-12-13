fa6ae0ec3370728f4b20bbf56f7f7c06ec66d5f8
Wasut <wasut_pao@hotmail.com>
2016-12-13 19:27:50 +0700

    add insert book schema script

---
+++ schema.js
@@ -0,0 +1,49 @@
+use bookstore;
+
+db.books.insert(
+	{
+		"id": 1,
+		"title": "Scala Cookbook",
+		"price": 250,
+		"author": [
+			"Christopher Noland",
+			"David"
+		]
+	}
+);
+
+db.books.insert(
+	{
+		"id": 2,
+		"title": "Java Guide",
+		"price": 350,
+		"author": [
+			"John Terry"
+		]
+	}
+);
+
+db.books.insert(
+	{
+		"id": 3,
+		"title": "Mongodb Practice",
+		"price": 1250,
+		"author": [
+			"Arya Stark",
+			"Rob Stark"
+		]
+	}
+);
+
+db.books.insert(
+	{
+		"id": 5,
+		"title": "Ruby on Rails",
+		"price": 550,
+		"author": [
+			"The Mountain",
+			"The Hound"
+		]
+	}
+);
+
