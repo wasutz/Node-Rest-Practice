a3c0e152fe358bb9a266ed554d94fe4cccde8ccb
Wasut <wasut_pao@hotmail.com>
2016-12-13 22:37:15 +0700

    add module, findById, createBook, updateBook, deleteBook

--- models/book.js
+++ models/book.js
@@ -18,3 +18,29 @@ var Book = module.exports = mongoose.model('Book', bookSchema);
 module.exports.findAll = function(callback, limit){
 	Book.find(callback).limit(limit);
 }
+
+module.exports.findById = function(id, callback){
+	Book.findOne({"_id": id }, callback);
+}
+
+module.exports.createBook = function(book, callback){
+	Book.create(book, callback);
+}
+
+module.exports.updateBook = function(id, newBook, callback){
+	Book.findOne({"_id": id }, function(err, book){
+		if(err){
+			throw err;
+		}
+
+		book.title = newBook.title;
+		book.price = newBook.price;
+		book.authors = newBook.authors;
+
+		book.save(callback);
+	});
+}
+
+module.exports.deleteBook = function(id, callback){
+	Book.remove({"_id": id }, callback);
+}
